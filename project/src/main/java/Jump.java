438655dec63c6553d1ac21b6b75f74d428e89bc1
kross <krossford@foxmail.com>
2018-03-14 16:46:08 +0800

    增加执行跳跃的时候，随机长按坐标

--- project/src/main/java/Jump.java
+++ project/src/main/java/Jump.java
@@ -1,3 +1,5 @@
+import kross.devtool.Log;
+
 import java.io.IOException;
 
 /**
@@ -5,10 +7,17 @@ import java.io.IOException;
  */
 public class Jump {
 
+    public static final String LOG_TAG = "Jump";
+
     public static void jump(float distance) {
+
         long millis = (long) (distance * 1.34);
+        int randomX = (int) (Math.random() * 500 + 100);
+        int randomY = (int) (Math.random() * 600 + 200);
+
+        Log.i(LOG_TAG, "距离: " + distance + " 长按: " + millis + "ms " + "按下坐标: " + randomX + ", " + randomY);
         try {
-            Runtime.getRuntime().exec("adb shell input touchscreen swipe 170 187 170 187 " + millis);
+            Runtime.getRuntime().exec(String.format("adb shell input touchscreen swipe %d %d %d %d %d", randomX, randomY, randomX, randomY, millis));
         } catch (IOException e) {
             e.printStackTrace();
         }
